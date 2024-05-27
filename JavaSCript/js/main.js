58e23bcfd0e362b3ea9901a6a51c1d370cf68403
kathepr <160674528+kathepr@users.noreply.github.com>
2024-05-27 08:08:19 -0500

    feat :sparkles: typeof para conocer el tipo de dato

--- JavaSCript/js/main.js
+++ JavaSCript/js/main.js
@@ -13,3 +13,17 @@ let noEsUnNumero = "hola" * 3.7; //NaN - No es un número
 console.log(noEsUnNumero);
 
 
+var text = "Hola, me llamo Kathe";
+var number = 30;
+var boolean = true;
+var notDefined; //undefined
+
+console.log(typeof text); //string
+console.log(typeof number); //number
+console.log(typeof boolean); //boolean
+console.log(typeof notDefined); //undefined
+
+
+//typeof nos devuelve el tipo de dato de la variable que le pasemos por parámetro
+
+// La función typeof() no nos servirá para variables con tipos de datos más complejos, ya que siempre los mostrará como object. Es  mejor utilizar constructor.name.
