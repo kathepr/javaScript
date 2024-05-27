d4ea4f9b89e15efed9c765a908b01db4d57869b2
kathepr <160674528+kathepr@users.noreply.github.com>
2024-05-27 08:00:55 -0500

    feat :sparkles: Se aprende la diferencia entre null, undefined y NaN

--- JavaSCript/js/main.js
+++ JavaSCript/js/main.js
@@ -1,4 +1,15 @@
 console.log("¡Hola Kathe!");
 
+let data = 5;
+console.log(typeof data);
+
+let indefinida; // Variable que no se le ha asignado un valor.
+console.log(indefinida);
+
+let nulo = null; //Le asigno intencionalmente null (variable vacia)
+console.log(nulo)
+
+let noEsUnNumero = "hola" * 3.7; //NaN - No es un número
+console.log(noEsUnNumero);
 
 
