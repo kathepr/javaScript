9aafe7f72036fd23208a641a5fd25c5e7557878b
kathepr <160674528+kathepr@users.noreply.github.com>
2024-06-06 08:06:57 -0500

    feat :sparkles: Add post

--- module/users.js
+++ module/users.js
@@ -8,7 +8,7 @@ const validateGetUser= async({userId}) => {
 export const getUser = async(arg) =>{
     let val = await validateGetUser(arg);
     if(val) return val;
-    let res = await fetch(`https://jsonplaceholder.typicode.com/users/${arg.userId}`)
+    let res = await fetch(`http://172.16.101.146:5800/users/${arg.userId}`)
     if(res.status === 404) return {status: 204, message: `Username does not exist`}
     let data = await res.json();
     return data
@@ -16,16 +16,42 @@ export const getUser = async(arg) =>{
 
 
 
+// Función para validar los datos del usuario
+const validateAddUser = async({ id, name, username, email, address, phone, website, company }) => {
+    if(typeof id !== "string" || id === undefined) 
+        return {status: 406, message: `El dato ${id} recibido no cumple con el tipo de dato número.`};
+    if(typeof name !== "string" || name === undefined) 
+        return {status: 406, message: `El dato ${name} recibido no cumple con el tipo de dato string.`};
+    if(typeof username !== "string" || username === undefined) 
+        return {status: 406, message: `El dato ${username} recibido no cumple con el tipo de dato string.`};
+    if(typeof email !== "string" || email === undefined) 
+        return {status: 406, message: `El dato ${email} recibido no cumple con el tipo de dato string.`};
+    if(typeof address !== "object" || address === undefined) 
+        return {status: 406, message: `El dato ${address} recibido no cumple con el tipo de dato object.`};
+    if(typeof phone !== "string" || phone === undefined) 
+        return {status: 406, message: `El dato ${phone} recibido no cumple con el tipo de dato string.`};
+    if(typeof website !== "string" || website === undefined) 
+        return {status: 406, message: `El dato ${website} recibido no cumple con el tipo de dato string.`};
+    if(typeof company !== "object" || company === undefined) 
+        return {status: 406, message: `El dato ${company} recibido no cumple con el tipo de dato object.`};
+    
+    let user = await getUser({ id });
+    if(user.status === 204) 
+        return {status: 200, message: `El usuario no existe`};
+}
 
-// export const addUser = async(arg)=>{
-//     let val = await addUser(arg);
-//     if(val) return val;
-//     let config = {
-//         method: "POST",
-//         headers: {"Content-Type": "application/json"},
-//         body: JSON.stringify(arg)
-//     }
-//     let res = await fetch("https://jsonplaceholder.typicode.com/users", config)
-//     let data = await res.json();
-//     return data;
-// }
+// Función para agregar un nuevo usuario
+export const addUser = async(arg) => {
+    let val = await validateAddUser(arg);
+    if(val) return val;
+
+    const config = {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(arg)
+    }
+
+    let res = await fetch("http://172.16.101.146:5804/users", config);
+    let data = await res.json();
+    return data;
+}
