a695b505b155badd725ced4b026dc45c19e48dbc
kathepr <160674528+kathepr@users.noreply.github.com>
2024-06-06 08:07:50 -0500

    feat :sparkles: Import modules

--- js/main.js
+++ js/main.js
@@ -1,25 +1,57 @@
-import { addAlbum } from "../module/album.js";
+import { getAllALbums, addAlbum, deleteAlbum } from "../module/album.js";
 import { addPost } from "../module/post.js";
 import { addTodos } from "../module/todos.js";
-// import {addUser} from "../module/users.js";
+import {addUser} from "../module/users.js";
+import { addComment } from "../module/comments.js";
 
 
 
 // console.table(await getAllALbums());
 // addAlbum({userId: 10, title:"Hello World"})
 
-console.table(await addAlbum({userId: 10, title:"Hello World"}));
+// console.table(await addAlbum({userId: "10", title:"lalala"}));
 
-console.table(await addPost({
-    "userId": 1,
-    "id": 1,
-    "title": "lala",
-    "body": "bla"
-  }))
+// console.table(await addPost({
+//     "userId": "1",
+//     "title": "lala",
+//     "body": "bla"
+//   }))
 
-console.table(await addTodos({
-    "userId": 10,
-    "id": 201,
-    "title": "lalala",
-    "completed": true
-}))
+// console.table(await addTodos({
+//   "userId": "10",
+//   "title": "lalala",
+//   "completed": true
+// }))
+
+// console.table(await addUser({
+//   "name": "Katheryn PR",
+//   "username": "kathepr",
+//   "email": "kathe@april.biz",
+//   "address": {
+//     "street": "Dreamers Street",
+//     "suite": "Second star to the right and straight on till morning",
+//     "city": "Where dreams come true.",
+//     "zipcode": "92998-3874",
+//     "geo":{
+//       "lat": "-37.3159",
+//       "lng": "81.1496"
+//     }
+//   },
+//   "phone": "1-770-736-8031 x56442",
+//   "website": "kathe.org",
+//   "company": {
+//     "name": "La la land",
+//     "catchPhrase": "Where dreams come true",
+//     "bs": "harness real-time dreams"
+//   }
+// }))
+
+// console.table(await addComment({
+//   "postId": "1",
+//   "name": "id labore ex et quam laborum",
+//   "email": "Eliseo@gardner.biz",
+//   "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+// }))
+
+
+// console.log(await deleteAlbum({id: "48b7"}))
