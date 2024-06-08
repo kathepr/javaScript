2b0ee0cb174ccef034c22ffe48707fcba78f5a91
kathepr <160674528+kathepr@users.noreply.github.com>
2024-06-08 18:02:32 -0500

    feat ✨ Se crea funcion patch en comments se modifica el idioma

--- module/comments.js
+++ module/comments.js
@@ -1,52 +1,65 @@
-import { getUser } from "./users.js"; // Lo importo para verificar el usuario
+import {getPost} from "./posts.js";
+export const getAllComments = async () => {
+    let res = await fetch("http://172.16.101.146:5801/comments");
+    let data = await res.json();
+    return data;
+};
 
-const validateAddComment = async({ postId, id, name, email, body }) => {
-    if (typeof postId !== "string" || postId === undefined) return {status: 406, message: `El dato ${postId} recibido no cumple con el tipo de dato ${typeof postId}.`};
-    if (typeof id !== "string" || id === undefined) return {status: 406, message: `El dato ${id} recibido no cumple con el tipo de dato ${typeof id}.`};
-    if (typeof name !== "string" || name === undefined) return {status: 406, message: `El dato ${name} recibido no cumple con el tipo de dato ${typeof name}.`};
-    if (typeof email !== "string" || email === undefined || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) return {status: 406, message: `El dato ${email} recibido no cumple con el tipo de dato ${typeof email} o el formato de email es incorrecto.`};
-    if (typeof body !== "string" || body === undefined) return {status: 406, message: `El dato ${body} recibido no cumple con el tipo de dato ${typeof body}.`};
-
-    // Validar que el usuario existe antes de agregar el comentario
-    let user = await getUser({ userId: postId }); 
-    if (user.status == 204) return {status: 200, message: `Username does not exist`};
-
+const validateAddComments = async ({ postId, name, email, body }) => {
+    if (typeof name !== "string" || name === undefined) return { status: 406, message: `The data for ${name} is either not arriving or does not comply with the required format.` }
+    if (typeof email !== "string" || email === undefined) return { status: 406, message: `The data for ${email} is either not arriving or does not comply with the required format.` }
+    if (typeof body !== "string" || body === undefined) return { status: 406, message: `The data for ${body} is either not arriving or does not comply with the required format.` }
+    let user = await getPost({ postId })
+    if (user.status == 204) return { status: 200, message: `Post doesn't exist` }
 }
 
-export const addComment = async(arg) => {
-    let val = await validateAddComment(arg);
+export const addComments = async (arg) => {
+    let val = await validateAddComments(arg);
     if (val) return val;
-
-    const config = {
+    let config = {
         method: "POST",
-        headers: {"Content-Type": "application/json"},
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(arg)
-    }
-
+    };
     let res = await fetch("http://172.16.101.146:5801/comments", config);
     let data = await res.json();
     return data;
-}
+};
 
-const validateDeleteComment = async ({id}) => {
-    if(typeof id !== "string" || id === undefined) return {status: 406, message: "El id del comentario no cumple con el tipo de dato."}
-}
-
-
-
-export const deleteComment = async(arg) => {
-    let val = await validateDeleteComment(arg);
-    if(val) return val;
-    
+export const updateComments = async (arg) => {
+    let val = await validateAddComments(arg);
+    if (val) return val;
+    let {id} = arg
     let config = {
-        method: "DELETE",
-        headers: {"Content-Type": "application/json"},
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(arg)
     };
-    
-    let res = await fetch(`http://172.16.101.146:5801/comments/${arg.id}`, config);
-    if (res.status === 404) return {status: 204, message: "El comentario que desea eliminar no está registrado"}
+    let res = await fetch(`http://172.16.101.146:5801/comments/${id}`, config);
     let data = await res.json();
-    data.status = 202
-    data.message = `El comentario ${arg.id} fue eliminado de la base de datos`
     return data;
 }
+
+export const patchComments = async (arg) => {
+    let {id} = arg;
+    let config = {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(arg)
+    };
+    let res = await fetch(`http://172.16.101.146:5801/comments/${id}`, config);
+    let data = await res.json();
+    return data;
+}
+
+export const deleteComments  = async(commentsId)=>{
+    let config = {  
+      method: "DELETE"
+    };
+    let res = await fetch(`http://172.16.101.146:5801/comments/${commentsId}`, config);
+    if(res.status ===404) return {status: 204, message:"The comment ID does not exist or is in an unaccepted format."}
+    let data = await res.json();
+    data.status = 202;
+    data.message =`The comment with ID ${commentsId} has been deleted from the database.`
+    return data;
+  }
